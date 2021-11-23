576aa9eed89d73fdf89e36c2c2b4493366a92f31
DESKTOP-47G9TA8\Andrii <andrsaps@gmail.com>
2021-11-23 07:01:09 +0200

    Fixed simplex method again

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -47,128 +47,17 @@ matrix.optimal_strategy()
 optimized = matrix.optimize()
 optimized
 
-class LinearModel:
-    def __init__(self, A=np.empty([0, 0]), b=np.empty([0, 0]), c=np.empty([0, 0]), minmax="MAX"):
-        self.A = A
-        self.b = b
-        self.c = c
-        self.x = [float(0)] * len(c)
-        self.minmax = minmax
-        self.printIter = True
-        self.optimalValue = None
-        self.transform = False
 
-    def printSoln(self):
-        print("\nОтримані y: ")
-        print(self.x)
-        print("Значення цільової функції: ")
-        print(self.optimalValue)
-        print("Знайдений вектор ймовірності для першого гравця можна записати у вигляді:\n P(0.125,0,0,0.875,0)")
-        print("Знайдений вектор ймовірності для другого гравця можна записати у вигляді:\n Q(0,",self.x[0],",",0,",",0,",",self.x[1],")")
+import pulp
+x1 = pulp.LpVariable("x1", lowBound=0)
+x2 = pulp.LpVariable("x2", lowBound=0)
+x3 = pulp.LpVariable("x3", lowBound=0)
 
-    def getTableau(self):
-        # починаємо роботу із tableau
-        if (self.minmax == "MIN" and self.transform == False):
-            self.c[0:len(c)] = -1 * self.c[0:len(c)]
-            self.transform = True
-
-        t1 = np.array([None, 0])
-        numVar = len(self.c)
-        numSlack = len(self.A)
-
-        t1 = np.hstack(([None], [0], self.c, [0] * numSlack))
-
-        basis = np.array([0] * numSlack)
-
-        for i in range(0, len(basis)):
-            basis[i] = numVar + i
-
-        A = self.A
-
-        if (not ((numSlack + numVar) == len(self.A[0]))):
-            B = np.identity(numSlack)
-            A = np.hstack((self.A, B))
-
-        t2 = np.hstack((np.transpose([basis]), np.transpose([self.b]), A))
-
-        tableau = np.vstack((t1, t2))
-
-        tableau = np.array(tableau, dtype='float')
-
-        return tableau
-
-    def optimize(self):
-
-        if (self.minmax == "MIN" and self.transform == False):
-            for i in range(len(self.c)):
-                self.c[i] = -1 * self.c[i]
-                transform = True
-
-        tableau = self.getTableau()
-        # якщо базис не вимагається
-        optimal = False
-        #трекаємо ітерації для виводу
-        iter = 1
-
-        while (True):
-
-            if (self.minmax == "MAX"):
-                for profit in tableau[0, 2:]:
-                    if profit > 0:
-                        optimal = False
-                        break
-                    optimal = True
-            else:
-                for cost in tableau[0, 2:]:
-                    if cost < 0:
-                        optimal = False
-                        break
-                    optimal = True
-
-            #якщо всі результати знаходяться у мінімальних витратах або максимальному заробітку
-            if optimal == True:
-                break
-
-            # nth змінних входять у базис, враховуючи індексацію для tableau
-            if (self.minmax == "MAX"):
-                n = tableau[0, 2:].tolist().index(np.amax(tableau[0, 2:])) + 2
-            else:
-                n = tableau[0, 2:].tolist().index(np.amin(tableau[0, 2:])) + 2
-
-            # тест на мінімум, тоді rth змінних виходять із базису
-            minimum = 1000
-            r = -1
-            for i in range(1, len(tableau)):
-                if (tableau[i, n] > 0):
-                    val = tableau[i, 1] / tableau[i, n]
-                    if val < minimum:
-                        minimum = val
-                        r = i
-
-            pivot = tableau[r, n]
-            # операція із рядками
-            # ділимо ведучу стрічку на ведучий елемент
-            tableau[r, 1:] = tableau[r, 1:] / pivot
-
-            # ділимо інші стрічки
-            for i in range(0, len(tableau)):
-                if i != r:
-                    mult = tableau[i, n] / tableau[r, n]
-                    tableau[i, 1:] = tableau[i, 1:] - mult * tableau[r, 1:]
-                    # нове значення базисної змінної
-            tableau[r, 0] = n - 2
-            iter += 1
-        self.x = np.array([0] * len(c), dtype=float)
-        # збергієамо коефіцієнти
-        for key in range(1, (len(tableau))):
-            if (tableau[key, 0] < len(c)):
-                self.x[int(tableau[key, 0])] = tableau[key, 1]
-
-        self.optimalValue = -1 * tableau[0, 1]
-
-b = np.ones(optimized.shape[0])
-c = np.ones(optimized.shape[1])
-model1 = LinearModel(optimized, b, c)
-
-model1.optimize()
-model1.printSoln()
+prob = pulp.LpProblem('Завдання', pulp.LpMaximize)
+prob += x1 + x2 + x3
+prob += 13*x1 + 10*x2 + 14*x3 <= 1
+prob += 12*x1 + 14*x2 + 10*x3 <= 1
+prob += 9*x1 + 11*x2 + 14*x3 <= 1
+prob.solve()
+[f'{v} = {v.varValue}' for v in prob.variables()]
+print(f'Результат: {pulp.value(prob.objective)}')
